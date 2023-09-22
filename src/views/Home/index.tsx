25d01eba667b9088d7a2538dc5dc79717484d1fb
Agustin <agustinescalanterojo@gmail.com>
2023-09-22 20:20:21 +0200

    feat: added header

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,10 +1,12 @@
 import { FC, memo } from 'react';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { Container } from './styles';
 
 const Home: FC = () => {
   return (
     <>
+      <Header />
       <Container>Psiqu&co</Container>
       <Footer />
     </>
