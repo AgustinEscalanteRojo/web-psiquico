90558f136444e01dd13a6061227b6ec999700914
Agustin <agustinescalanterojo@gmail.com>
2023-09-26 16:11:14 +0200

    refactor: added new routes to the router

--- src/containers/Router/index.tsx
+++ src/containers/Router/index.tsx
@@ -1,16 +1,24 @@
-import { FC } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from '../../views/Home'
+import { FC } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from '../../views/Home';
+import Consult from '../../views/Consult';
+import Contact from '../../views/Contact';
+import Who from '../../views/Who';
+import Location from '../../views/Location';
 
 const Router: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Home" element={<Home/>} />
+        <Route path="/Consult" element={<Consult />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Location" element={<Location />} />
+        <Route path="/Who" element={<Who />} />
         <Route path="/*" element={<Navigate replace to="/Home" />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
