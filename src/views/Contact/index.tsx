c7de3bbba6c2fcad389f8e5a52303502f74669d2
Agustin <agustinescalanterojo@gmail.com>
2023-10-03 18:37:16 +0200

    feat: add contact view data

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -2,13 +2,44 @@ import { FC, memo } from 'react';
 import Header from '../../components/Header';
 import Background from '../../components/Background';
 import Footer from '../../components/Footer';
-import { Container } from './styles';
+import { Container, ContactInfo, Icon } from './styles';
 
 const Contact: FC = () => {
   return (
     <>
       <Header />
-      <Container></Container>
+      <Container>
+        <h1>Contacto</h1>
+        <h3>María Marín Domínguez</h3>
+        <ContactInfo>
+          <Icon>Email:</Icon> maria@psiquico.com
+        </ContactInfo>
+        <ContactInfo>
+          <Icon>Teléfono:</Icon> 666 666 666
+        </ContactInfo>
+        <ContactInfo>
+        <Icon>Linkedin:</Icon> enlace linkedin
+        </ContactInfo>
+        <ContactInfo>
+        <Icon>Twitter:</Icon> @marinpsicologa
+        </ContactInfo>
+
+        <h3>Felix Valderrama Díaz</h3>
+        <ContactInfo>
+          <Icon>Email:</Icon> felix@psiquico.com
+        </ContactInfo>
+        <ContactInfo>
+          <Icon>Teléfono:</Icon> 777 777 777 
+        </ContactInfo>
+        <ContactInfo>
+          <Icon>Linkedin:</Icon> enlace linkedin
+        </ContactInfo>
+        <ContactInfo>
+          <Icon>Twitter:</Icon> @felixvalderrama
+        </ContactInfo>
+
+        
+      </Container>
       <Background />
       <Footer />
     </>
--- src/views/Contact/styles.ts
+++ src/views/Contact/styles.ts
@@ -3,5 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center; /* Centrar horizontalmente en el contenedor principal */
+  align-items: center;
+`;
+
+export const ContactInfo = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const Icon = styled.div`
+  margin-right: 5px;
+  /* Agrega estilos para el icono aquí */
 `;
