b3ef568b7b480b9a96420ca96ee565f5d905b091
Agustin <agustinescalanterojo@gmail.com>
2023-10-04 18:16:29 +0200

    feat: add icons to contact view

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -2,6 +2,9 @@ import { FC, memo } from 'react';
 import Header from '../../components/Header';
 import Background from '../../components/Background';
 import Footer from '../../components/Footer';
+import { RiTwitterXFill } from 'react-icons/ri';
+import { BsFillTelephoneForwardFill, BsLinkedin } from 'react-icons/bs';
+import { MdAttachEmail } from 'react-icons/md';
 import { Container, ContactInfo, Icon } from './styles';
 
 const Contact: FC = () => {
@@ -12,33 +15,35 @@ const Contact: FC = () => {
         <h1>Contacto</h1>
         <h3>María Marín Domínguez</h3>
         <ContactInfo>
-          <Icon>Email:</Icon> maria@psiquico.com
+          <MdAttachEmail />
+          Email: maria@psiquico.com
         </ContactInfo>
         <ContactInfo>
-          <Icon>Teléfono:</Icon> 666 666 666
+          <BsFillTelephoneForwardFill />
+          Teléfono: 666 666 666
         </ContactInfo>
         <ContactInfo>
-        <Icon>Linkedin:</Icon> enlace linkedin
+          <BsLinkedin /> Linkedin: enlace linkedin
         </ContactInfo>
         <ContactInfo>
-        <Icon>Twitter:</Icon> @marinpsicologa
+          <RiTwitterXFill /> Twitter: @marinpsicologa
         </ContactInfo>
 
         <h3>Felix Valderrama Díaz</h3>
         <ContactInfo>
-          <Icon>Email:</Icon> felix@psiquico.com
+        <MdAttachEmail />
+          Email: felix@psiquico.com</ContactInfo>
+        <ContactInfo>
+        <BsFillTelephoneForwardFill />
+          Teléfono: 777 777 777</ContactInfo>
+        <ContactInfo>
+          <BsLinkedin />
+          Linkedin: enlace linkedin
         </ContactInfo>
         <ContactInfo>
-          <Icon>Teléfono:</Icon> 777 777 777 
+          <RiTwitterXFill />
+          Twitter: @felixvalderrama
         </ContactInfo>
-        <ContactInfo>
-          <Icon>Linkedin:</Icon> enlace linkedin
-        </ContactInfo>
-        <ContactInfo>
-          <Icon>Twitter:</Icon> @felixvalderrama
-        </ContactInfo>
-
-        
       </Container>
       <Background />
       <Footer />
