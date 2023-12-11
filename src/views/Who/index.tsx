fa298be30fb41fdcd704148aec1421466040ce0c
Agustin <agustinescalanterojo@gmail.com>
2023-12-11 21:36:51 +0100

    add styles to profile

--- src/views/Who/index.tsx
+++ src/views/Who/index.tsx
@@ -47,36 +47,49 @@ const Who: FC = () => {
                   graduarme en Psicología por la Universidad Nacional de
                   Educación a Distancia (UNED) y habilitarme como psicóloga
                   sanitaria con el Máster en Psicología General Sanitaria por la
-                  UNIR,continúe mi formación enfocada en la intervención y
+                  UNIR, continúe mi formación enfocada en la intervención y
                   prevención de adicciones, la violencia de género, el trauma y
-                  personalidad. Para poder ofrecer un excelente tratamiento a
-                  mis pacientes, mantengo mis conocimientos en continua
-                  actualización. Pudiendo así ofrecer la terapia que cuente más
-                  evidencia empírica. Mi formación como psicóloga sanitaria la
-                  he completado especializándome en las áreas que más interés me
-                  suscitan cursando para ello el Experto Universitario en
-                  Intervención en Trauma, la Especidad en psicología legal y
-                  forense, en Intervención en adicciones y como Agente en
-                  Violencia de Género. Además de múltiples cursos relacionados
-                  con esta temática y asistencia a congresos. En mi enfoque
-                  integrador, con una metodología en la que aplico las técnicas
-                  más adecuadas según las necesidades de cada individuo, prima
-                  la terapia Cognitivo-conductual entre otras, y las
+                  personalidad.
+                  <br />
+                  <br />
+                  Para poder ofrecer un excelente tratamiento a mis pacientes,
+                  mantengo mis conocimientos en continua actualización. Pudiendo
+                  así ofrecer la terapia que cuente más evidencia empírica. Mi
+                  formación como psicóloga sanitaria la he completado
+                  especializándome en las áreas que más interés me suscitan
+                  cursando para ello el Experto Universitario en Intervención en
+                  Trauma, la Especialidad en psicología legal y forense, en
+                  Intervención en adicciones y como Agente en Violencia de
+                  Género. Además de múltiples cursos relacionados con esta
+                  temática y asistencia a congresos.
+                  <br />
+                  <br />
+                  En mi enfoque integrador, con una metodología en la que aplico
+                  las técnicas más adecuadas según las necesidades de cada
+                  individuo, prima la terapia{' '}
+                  <strong>Cognitivo-conductual</strong> entre otras, y las
                   intervenciones están adaptadas para satisfacer la demanda
-                  particular de cada caso. En 2015 comenzó mi andadura en el
-                  tratamiento de las adicciones, desde 2018 fui involucrándome
-                  en la atención a víctimas de Violencia de Género teniendo la
-                  oportunidad de participar en programas de la Cruz Roja e
-                  interviniendo en estudios sobre esto en otras asociaciones.
-                  Estos dos ámbitos profesionales que tanto me apasionan, me
-                  llevaron a interesarme por la perspectiva de género en el
-                  tratamiento de las adicciones y hoy en día es una variable a
-                  considerar en el tratamiento que ofrezco en adicciones. En
-                  2021, tras muchos años de ir dando forma a nuestra idea de
+                  particular de cada caso.
+                  <br />
+                  <br />
+                  En 2015 comenzó mi andadura en el tratamiento de las
+                  adicciones, desde 2018 fui involucrándome en la atención a
+                  víctimas de Violencia de Género teniendo la oportunidad de
+                  participar en programas de la Cruz Roja e interviniendo en
+                  estudios sobre esto en otras asociaciones. Estos dos ámbitos
+                  profesionales que tanto me apasionan, me llevaron a
+                  interesarme por la perspectiva de género en el tratamiento de
+                  las adicciones y hoy en día es una variable a considerar en el
+                  tratamiento que ofrezco en adicciones.
+                  <br />
+                  <br />
+                  En 2021, tras muchos años de ir dando forma a nuestra idea de
                   centro sanitario de psicología nació PSIQUI&CO. Un centro en
                   el que ejerzo como psicóloga y puedo hacer realidad mi gran
                   pasión, en este espacio seguro atiendo de forma individual, de
                   pareja o familias a todas aquellos adultos que lo necesiten.
+                  <br />
+                  <br />
                   Creo firmemente en la importancia de la empatía y la conexión
                   genuina en el proceso terapéutico. Acompañar a mis pacientes
                   para lograr un mayor autoconocimiento y empoderamiento es la
@@ -172,43 +185,58 @@ const Who: FC = () => {
                   Psicología por la Universidad Nacional de Educación a
                   Distancia (UNED), enfocándome en el área de Psicología
                   Clínica, Personalidad, Evaluación y Tratamientos Psicológicos,
-                  además del área de psicología educativa. Continué mi formación
-                  con un Máster en Psicología General Sanitaria (UNIR),
-                  consolidando mis conocimientos y habilidades para ofrecer una
-                  atención integral a aquellos que buscan apoyo psicológico.
+                  además del área de psicología educativa.
+                  <br />
+                  <br />
+                  Continué mi formación con un Máster en Psicología General
+                  Sanitaria (UNIR), consolidando mis conocimientos y habilidades
+                  para ofrecer una atención integral a aquellos que buscan apoyo
+                  psicológico.
+                  <br />
+                  <br />
                   Desde 2019, he estado comprometido en el ámbito de la
                   protección de menores, desempeñando roles de psicólogo en
                   diversas entidades del Sistema de Protección de Menores y en
                   Centros de Internamiento de Menores Infractores. También he
                   contribuido como Psicólogo Voluntario en programas de Cruz
-                  Roja, brindando mi ayuda en diferentes iniciativas. En 2021,
-                  materializamos el sueño de crear el Centro Sanitario
+                  Roja, brindando mi ayuda en diferentes iniciativas.
+                  <br />
+                  <br />
+                  En 2021, materializamos el sueño de crear el Centro Sanitario
                   PSIQUI&CO, donde actualmente ejerzo como psicólogo sanitario.
                   En este espacio, comparto experiencias junto a otras
                   profesionales, ofrezco consultas individuales, de pareja y
-                  familia para menores y adultos A lo largo de mi carrera, he
-                  mantenido un compromiso constante con mi desarrollo
-                  profesional, participando activamente en cursos, talleres,
-                  congresos y seminarios especializados en Psicología Clínica y
-                  de la Salud, Dirección de Psicodrama, y obteniendo
-                  certificaciones como Experto Universitario en violencia filio
-                  parental, Experto Universitario en Intervención con menores,
-                  Protección y conflicto con la Ley, Experto Universitario en el
-                  perito judicial, social, educativo y psicológico, y
-                  Especialista Universitario en Intervención en Trauma. Creo
-                  firmemente que la práctica de la psicología requiere un
+                  familia para menores y adultos.
+                  <br />
+                  <br />
+                  A lo largo de mi carrera, he mantenido un compromiso constante
+                  con mi desarrollo profesional, participando activamente en
+                  cursos, talleres, congresos y seminarios especializados en
+                  Psicología Clínica y de la Salud, Dirección de Psicodrama, y
+                  obteniendo certificaciones como Experto Universitario en
+                  violencia filio parental, Experto Universitario en
+                  Intervención con menores, Protección y conflicto con la Ley,
+                  Experto Universitario en el perito judicial, social, educativo
+                  y psicológico, y Especialista Universitario en Intervención en
+                  Trauma.
+                  <br />
+                  <br />
+                  Creo firmemente que la práctica de la psicología requiere un
                   compromiso continuo con la formación y la actualización, dado
                   su constante proceso de evolución. Por esta razón, me esfuerzo
                   por mantenerme al día con los últimos avances en psicología,
                   garantizando así un servicio de calidad basado en el
                   conocimiento más actualizado.Mi orientación y metodología de
-                  trabajo podría definirse como Psicodrama y Cognitivo
-                  Conductual / Contextual. En la práctica clínica integro
-                  estrategias y técnicas terapéuticas provenientes de distintas
-                  orientaciones psicológicas que han mostrado su eficacia
-                  (siempre desde una óptica científica, avalada por los datos
-                  empíricos). Estoy aquí para acompañarte en tu viaje hacia el
-                  bienestar emocional y el crecimiento personal.
+                  trabajo podría definirse como{' '}
+                  <strong>Psicodrama y Cognitivo Conductual </strong>/
+                  Contextual. En la práctica clínica integro estrategias y
+                  técnicas terapéuticas provenientes de distintas orientaciones
+                  psicológicas que han mostrado su eficacia (siempre desde una
+                  óptica científica, avalada por los datos empíricos).
+                  <br />
+                  <br />
+                  Estoy aquí para acompañarte en tu viaje hacia el bienestar
+                  emocional y el crecimiento personal.
                 </StyledTypography>
               </div>
 
