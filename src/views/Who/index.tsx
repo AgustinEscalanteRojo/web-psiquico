901aac078a23f99f4e3737aabca1bf540c5fd31b
Agustin <agustinescalanterojo@gmail.com>
2024-05-02 16:44:15 +0200

    feat: add new psychologist to vista who

--- src/views/Who/index.tsx
+++ src/views/Who/index.tsx
@@ -302,6 +302,152 @@ const Who: FC = () => {
               </div>
             </BioContainer>
           </Grid>
+
+
+          <Grid item xs={12} md={6}>
+            <BioContainer>
+              <Title>Felix Valderrama Díaz</Title>
+              <img src="/Felix.jpg" alt="Felix" />
+
+              <div>
+                <StyledTypography variant="subtitle1">
+                  - Psicólogo General sanitario. Nº colegiado Psicólogo: AN
+                  10259
+                </StyledTypography>
+                <StyledTypography variant="subtitle1">
+                  - Director de Psicodrama / Psicodramatista Asociación Española
+                  de Psicodrama.
+                </StyledTypography>
+              </div>
+
+              <div>
+                <Subtitle1>Sobre mí</Subtitle1>
+                <StyledTypography variant="body1">
+                  Soy un psicólogo con pasión por la psicología. Graduado en
+                  Psicología por la Universidad Nacional de Educación a
+                  Distancia (UNED), enfocándome en el área de Psicología
+                  Clínica, Personalidad, Evaluación y Tratamientos Psicológicos,
+                  además del área de psicología educativa.
+                  <br />
+                  <br />
+                  Continué mi formación con un Máster en Psicología General
+                  Sanitaria (UNIR), consolidando mis conocimientos y habilidades
+                  para ofrecer una atención integral a aquellos que buscan apoyo
+                  psicológico.
+                  <br />
+                  <br />
+                  Desde 2019, he estado comprometido en el ámbito de la
+                  protección de menores, desempeñando roles de psicólogo en
+                  diversas entidades del Sistema de Protección de Menores y en
+                  Centros de Internamiento de Menores Infractores. También he
+                  contribuido como Psicólogo Voluntario en programas de Cruz
+                  Roja, brindando mi ayuda en diferentes iniciativas.
+                  <br />
+                  <br />
+                  En 2021, materializamos el sueño de crear el Centro Sanitario
+                  PSIQUI&CO, donde actualmente ejerzo como psicólogo sanitario.
+                  En este espacio, comparto experiencias junto a otras
+                  profesionales, ofrezco consultas individuales, de pareja y
+                  familia para menores y adultos.
+                  <br />
+                  <br />
+                  A lo largo de mi carrera, he mantenido un compromiso constante
+                  con mi desarrollo profesional, participando activamente en
+                  cursos, talleres, congresos y seminarios especializados en
+                  Psicología Clínica y de la Salud, Dirección de Psicodrama, y
+                  obteniendo certificaciones como Experto Universitario en
+                  violencia filio parental, Experto Universitario en
+                  Intervención con menores, Protección y conflicto con la Ley,
+                  Experto Universitario en el perito judicial, social, educativo
+                  y psicológico, y Especialista Universitario en Intervención en
+                  Trauma.
+                  <br />
+                  <br />
+                  Creo firmemente que la práctica de la psicología requiere un
+                  compromiso continuo con la formación y la actualización, dado
+                  su constante proceso de evolución. Por esta razón, me esfuerzo
+                  por mantenerme al día con los últimos avances en psicología,
+                  garantizando así un servicio de calidad basado en el
+                  conocimiento más actualizado.Mi orientación y metodología de
+                  trabajo podría definirse como{' '}
+                  <strong>Psicodrama y Cognitivo Conductual </strong>/
+                  Contextual. En la práctica clínica integro estrategias y
+                  técnicas terapéuticas provenientes de distintas orientaciones
+                  psicológicas que han mostrado su eficacia (siempre desde una
+                  óptica científica, avalada por los datos empíricos).
+                  <br />
+                  <br />
+                  Estoy aquí para acompañarte en tu viaje hacia el bienestar
+                  emocional y el crecimiento personal.
+                </StyledTypography>
+              </div>
+
+              <div>
+                <Subtitle1>Me puedes contactar en:</Subtitle1>
+                <StyledContainer>
+                  <ContactInfo>
+                    <MdAttachEmail style={{ color: '#ff5733' }} />
+                    <strong>Email: </strong>{' '}
+                    <a href="psicologofelixvalderramadiaz@gmail.com">
+                      psicologofelixvalderramadiaz@gmail.com
+                    </a>
+                  </ContactInfo>
+
+                  <ContactInfo>
+                    <FcCallback />
+                    <strong>Teléfono: </strong> 627 060 203
+                  </ContactInfo>
+
+                  <ContactInfo>
+                    <BsLinkedin style={{ color: '#0e76a8' }} />
+                    <a
+                      href="https://es.linkedin.com/in/felixvalderramadiaz"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <strong>Linkedin </strong>{' '}
+                    </a>
+                  </ContactInfo>
+
+                  <ContactInfo>
+                    <FaFacebookSquare style={{ color: '#1da1f2' }} />
+                    <a
+                      href="https://www.facebook.com/felix.valderramadiaz/?locale=es_LA"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <strong>Facebook</strong>{' '}
+                    </a>
+                  </ContactInfo>
+
+                  <ContactInfo>
+                    <FaUserDoctor style={{ color: '#1b907e' }} />
+                    <a
+                      href="https://www.doctoralia.es/felix-valderrama-diaz/psicologo-psicologo-infantil-terapeuta-complementario/sevilla"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <strong>Doctoralia </strong>{' '}
+                    </a>
+                  </ContactInfo>
+
+                  <ContactInfo>
+                    <FaUserDoctor style={{ color: '#1d1b90' }} />
+                    <a
+                      href="https://www.doctoralia.es/felix-valderrama-diaz/psicologo-psicologo-infantil-terapeuta-complementario/sevilla"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <strong>TopDoctors </strong>{' '}
+                    </a>
+                  </ContactInfo>
+                </StyledContainer>
+              </div>
+            </BioContainer>
+          </Grid>
+
+
+          
         </Grid>
       </StyledContainer>
 
